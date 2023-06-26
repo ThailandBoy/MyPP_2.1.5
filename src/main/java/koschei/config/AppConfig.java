e0d54ab4d30b4a0c493f70d4d5cadcd7a7a383ea
ThailandBoy <91521829+ThailandBoy@users.noreply.github.com>
2023-06-26 07:54:41 +0300

    It is realy nice exercise!

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,6 +1,9 @@
 package koschei.config;
 
+import koschei.models.Deth8;
+import koschei.models.Egg6;
 import koschei.models.Island2;
+import koschei.models.Needle7;
 import koschei.models.Wood3;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,4 +17,20 @@ public class AppConfig {
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
     }
+
+    @Bean
+    public static Egg6 getEgg6(Needle7 needle7) {
+        return new Egg6(needle7);
+    }
+
+    @Bean
+    public static Needle7 getNeedle7(Deth8 deth8) {
+        return new Needle7(deth8);
+    }
+
+    @Bean
+    public static Deth8 getDeth8() {
+        return new Deth8();
+    }
+
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,14 @@ package koschei.models;
 
 public class Egg6 {
 
+    private Needle7 needle7;
+
+    public Egg6(Needle7 needle72) {
+        this.needle7 = needle72;
+    }
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle7.toString();
     }
 }
